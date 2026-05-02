d408e59a5f29279a20c2cb87eb4ec11b3b00b681
lytree <hi.yangzh@outlook.com>
2026-05-02 11:10:32 +0800

    add Plugin

--- Avalonia.Plugin.Template/TemplatePlugin.cs
+++ Avalonia.Plugin.Template/TemplatePlugin.cs
@@ -11,7 +11,7 @@ public partial class TemplatePlugin : IPluginMetadata
     public string Author => "AvaloniaTemplate";
     public string Description => "A minimal template plugin demonstrating the plugin system with ViewModel-View binding and menu integration.";
     public IEnumerable<string> Dependencies => [];
-    public string PluginId => "Avalonia.Plugin.Template";
+    public string PluginId => "b5eab285-8673-4991-a45a-b43bee2cb840";
 
     public void Initialize()
     {
